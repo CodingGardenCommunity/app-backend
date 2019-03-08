e4a393f9235bc06e56f6758868763d651eefb81b
Yosef Spira <spira.joe214@gmail.com>
2019-03-08 09:44:51 -0500

    :art: Added contributors route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +8,12 @@ app.get('/', (req, res) => {
   res.json({
     message: '👋🌎',
   });
+});
+
+app.get('/contributors', async (req, res) => {
+  const response = await fetch('https://raw.githubusercontent.com/CodingGardenCommunity/contributors/master/contributors.json');
+  const data = await response.json();
+  res.json(data);
 });
 
 app.listen(port, () => console.log(`Server starting on http://localhost:${port}`));
