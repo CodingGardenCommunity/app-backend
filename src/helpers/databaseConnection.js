966a20c616d94db0579184e892fdb5cc1ade0b3d
Eckhardt (Kaizen) Dreyer <support@kaizenmedia.co.za>
2019-03-10 01:18:56 +0200

    Setup initial folder structure, dev env, local db & config (#10)
    * Setup initial folder structure, development env, local database and configuration
    
    * Adjust folder structure to spec
    
    * Fix linting errors
    
    * Install dependencies with yarn :sparkles:

---
+++ src/helpers/databaseConnection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { database } = require('../config');
+
+module.exports = mongoose.connect(
+  database.uri,
+  { useNewUrlParser: true },
+  () => process.stdout.write('\x1b[33m Connected to database \x1b[0m \n\n'),
+);
