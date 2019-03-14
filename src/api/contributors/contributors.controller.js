4c5cbde6cc06392f20b2f42fe4ac52bf2533e857
Yosef Spira <spira.joe214@gmail.com>
2019-03-14 16:30:47 -0400

    :art: removed irrelevant changes

--- src/api/contributors/contributors.controller.js
+++ src/api/contributors/contributors.controller.js
@@ -5,46 +5,7 @@ module.exports = async function getContributors(req, res) {
   try {
     const response = await fetch(contribURL);
     const data = await response.json();
-    const finalResponse = {
-      data: data
-        .sort((a, b) => new Date(a.joined) - new Date(b.joined))
-        .map(({
-          name,
-          github: username,
-          image,
-          countryCode,
-          teamIds,
-        }, key) => ({
-          type: 'contributor',
-          id: key,
-          attributes: {
-            username,
-            name,
-            image,
-            countryCode,
-          },
-          relationships: {
-            contributionArea: {
-              links: {
-                self: `/contributors/${key}/relationships/contribution-areas`,
-                related: `/contributors/${key}/contribution-areas`,
-              },
-              data: teamIds.map(team => ({
-                type: 'contribution_area',
-                id: team,
-              })),
-            },
-          },
-        })),
-      included: [
-        { type: 'contribution-area', id: '1', attributes: { name: 'Planning' } },
-        { type: 'contribution-area', id: '2', attributes: { name: 'DevOps' } },
-        { type: 'contribution-area', id: '3', attributes: { name: 'Frontend' } },
-        { type: 'contribution-area', id: '4', attributes: { name: 'Backend' } },
-        { type: 'contribution-area', id: '5', attributes: { name: 'Testing' } },
-      ],
-    };
-    return res.json(finalResponse);
+    return res.json(data);
   } catch ({ message }) {
     return res.status(400).json({ status: 400, message });
   }
