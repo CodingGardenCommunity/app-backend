5a13c43d876bffedbf80a31ca6ec8da2ceb0f13d
Semjon Bibow <semjon.bibo@cgi.com>
2019-03-17 16:32:53 +0100

    Set env vars correctly

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const app = express();
 app.use(express.json());
 
 // App routes
-app.get('/', (req, res) => res.json({ 
+app.get('/', (req, res) => res.json({
   msg: 'Check out /contributors and /faq',
-  'mongo-url': process.env.MONGO_URL,
+  'mongo-uri': process.env.MONGO_URI,
   'mongo-secret': process.env.MONGO_SECRET,
   'node-env': process.env.NODE_ENV,
 }));
