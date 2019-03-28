026babaeca50094452339071c8a90c484f2a2a86
spiray <spira.joe214@gmail.com>
2019-03-28 13:01:02 -0400

    :art: moved handler to dbConnect file

--- src/helpers/databaseConnection.js
+++ src/helpers/databaseConnection.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
 const { database: { uri } } = require('../config');
 
-module.exports = mongoose.connect(uri, { useNewUrlParser: true });
+module.exports = mongoose.connect(uri, { useNewUrlParser: true })
+  .then(
+    () => process.stdout.write('\x1b[33m Connected to database \x1b[0m \n\n'),
+    err => process.stdout.write(`\x1b[33m ${err} \x1b[0m \n\n`),
+  );
