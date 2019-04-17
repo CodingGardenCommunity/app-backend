f44bbfeeba36be687ce150cd8f0f2bdec0be4ba4
spiray <spira.joe214@gmail.com>
2019-04-17 15:11:05 -0400

    :art: Added post and delete routes to the FAQ router to handle deleteing FAQs and adding FAQs

--- src/api/faq/faq.controller.js
+++ src/api/faq/faq.controller.js
@@ -43,4 +43,42 @@ async function getFAQ(req, res) {
   }
 }
 
-module.exports = { getFAQ };
+async function addFAQ(req, res) {
+  try {
+    const { question, answer } = req.body;
+    if (!question || !answer) throw new ReferenceError('Make sure your request includes a question and answer.');
+    await new FAQ({ question, answer }).save();
+    return res.json({ message: 'FAQ added successfully to DB.' });
+  } catch (error) {
+    const {
+      message,
+    } = error;
+    const status = error instanceof ReferenceError ? 404 : 500;
+
+    return res.status(status).json({
+      status,
+      message,
+    });
+  }
+}
+
+async function removeFAQ(req, res) {
+  try {
+    const { id: _id } = req.params;
+    const deletedFAQ = await FAQ.deleteOne({ _id });
+    if (deletedFAQ.deletedCount === 0) throw new ReferenceError('There is no FAQ to delete with that ID.');
+    return res.json({ message: 'FAQ removed successfully from DB.' });
+  } catch (error) {
+    const {
+      message,
+    } = error;
+    const status = error instanceof ReferenceError ? 404 : 500;
+
+    return res.status(status).json({
+      status,
+      message,
+    });
+  }
+}
+
+module.exports = { getFAQ, addFAQ, removeFAQ };
--- src/api/faq/faq.routes.js
+++ src/api/faq/faq.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { getFAQ } = require('./faq.controller');
+const { getFAQ, addFAQ, removeFAQ } = require('./faq.controller');
 
 router.get('/', getFAQ);
 router.get('/:id', getFAQ);
-
+router.post('/add', addFAQ);
+router.delete('/delete/:id', removeFAQ);
 
 module.exports = router;
