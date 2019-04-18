faf7a540798d3fccf290a7c94981164576d7e917
spiray <spira.joe214@gmail.com>
2019-04-18 13:59:02 -0400

    changed route paths

--- src/api/faq/faq.routes.js
+++ src/api/faq/faq.routes.js
@@ -3,7 +3,7 @@ const { getFAQ, addFAQ, removeFAQ } = require('./faq.controller');
 
 router.get('/', getFAQ);
 router.get('/:id', getFAQ);
-router.post('/add', addFAQ);
-router.delete('/delete/:id', removeFAQ);
+router.post('/', addFAQ);
+router.delete('/:id', removeFAQ);
 
 module.exports = router;
