f46f7c82b8bcec0c5c0a69b708f2de52ce02b8aa
w3cj <cj@null.computer>
2019-04-19 08:23:28 -0600

    :bug: Update seed command for deployment

--- src/tasks/seed.js
+++ src/tasks/seed.js
@@ -1,7 +1,7 @@
 const seedFAQs = require('./seedFaq');
 
 (async () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && process.env.MONGO_URI) {
     await seedFAQs();
     process.exit(0);
   }
