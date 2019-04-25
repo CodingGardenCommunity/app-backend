f481b8587a674a6029e1dd456db0c90feb128d82
spiray <spira.joe214@gmail.com>
2019-04-25 13:13:10 -0400

    merging

--- src/tasks/seed.js
+++ src/tasks/seed.js
@@ -6,12 +6,8 @@ const seedFAQs = require('./seedFaq');
   try {
     await seedFAQs();
   } catch (error) {
-<<<<<<< HEAD
-    process.stdout.write(error);
-=======
     // eslint-disable-next-line no-console
     console.log(error);
->>>>>>> 05c1f18ff2db6fd3bbe844ac492eb0dbbf2a79a0
   } finally {
     process.exit(0);
   }
