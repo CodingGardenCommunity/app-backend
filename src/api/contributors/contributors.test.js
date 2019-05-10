7443ed5fc063ad8e7b586c4a911ed4091df93e92
Akash Rajpurohit <akash009.dev@gmail.com>
2019-05-10 20:02:21 +0530

    :sparkles: tested contributors routes

--- src/api/contributors/contributors.test.js
+++ src/api/contributors/contributors.test.js
@@ -9,3 +9,15 @@ describe('GET /contributors', () => {
       .expect('Content-Type', /json/)
       .expect(200));
 });
+
+describe('GET /contributors/:id', () => {
+	it('Should respond with a 200 status code for valid id', () => request(app)
+			.get('/contributors/w3cj')
+			.expect('Content-Type', /json/)
+			.expect(200));
+
+	it('Should respond with 404 status code for invalid id', () => request(app)
+			.get('/contributors/someInvalidID')
+			.expect('Content-Type', /json/)
+			.expect(404));
+});
