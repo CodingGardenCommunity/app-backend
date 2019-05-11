dc56dc3fe09e58d31d6846101c9cf6a8e04b973d
Akash Rajpurohit <akash009.dev@gmail.com>
2019-05-11 16:47:53 +0530

    :tada: tabs changed to 2 spaces

--- src/api/contributors/contributors.test.js
+++ src/api/contributors/contributors.test.js
@@ -3,22 +3,22 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('GET /contributors', () => {
-  it('Should respond with a 200 status code',
-    () => request(app)
-      .get('/contributors')
-      .expect('Content-Type', /json/)
-      .expect(200));
+	it('Should respond with a 200 status code',
+	  () => request(app)
+	  .get('/contributors')
+	  .expect('Content-Type', /json/)
+	  .expect(200));
 });
 
 describe('GET /contributors/:id', () => {
 	it('Should respond with a 200 status code for valid id', () => request(app)
-			.get('/contributors/w3cj')
-			.expect('Content-Type', /json/)
-			.expect(200));
+		.get('/contributors/w3cj')
+		.expect('Content-Type', /json/)
+		.expect(200));
 
 	it('Should respond with 404 status code for invalid id', () => request(app)
-			.get('/contributors/someInvalidID')
-			.expect('Content-Type', /json/)
-			.expect(404));
+		.get('/contributors/someInvalidID')
+		.expect('Content-Type', /json/)
+		.expect(404));
 });
 
