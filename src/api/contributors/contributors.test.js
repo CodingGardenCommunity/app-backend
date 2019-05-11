b6deb199f16fdcde1d9ee0dc5cce9a2a9a703002
Akash Rajpurohit <akash009.dev@gmail.com>
2019-05-11 17:23:42 +0530

    :sparkles: testing contributors routes along with body from response; changed format a little

--- src/api/contributors/contributors.test.js
+++ src/api/contributors/contributors.test.js
@@ -3,22 +3,28 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('GET /contributors', () => {
-	it('Should respond with a 200 status code',
-	  () => request(app)
-	  .get('/contributors')
-	  .expect('Content-Type', /json/)
-	  .expect(200));
+	it('Should respond with a 200 status code', async () => {
+		const res = await request(app).get('/contributors')
+		expect(res.status).toEqual(200)
+		expect(res.type).toEqual("application/json")
+		expect(res.body.data.length).toBeGreaterThanOrEqual(0)
+	})
 });
 
 describe('GET /contributors/:id', () => {
-	it('Should respond with a 200 status code for valid id', () => request(app)
-		.get('/contributors/w3cj')
-		.expect('Content-Type', /json/)
-		.expect(200));
+	it('Should respond with a 200 status code for valid id', async () => {
+		const res = await request(app).get('/contributors/w3cj')
+		expect(res.status).toEqual(200)
+		expect(res.type).toEqual("application/json")
+		expect(res.body.data[0].id).toEqual("w3cj")
 
-	it('Should respond with 404 status code for invalid id', () => request(app)
-		.get('/contributors/someInvalidID')
-		.expect('Content-Type', /json/)
-		.expect(404));
+	})
+
+	it('Should respond with 404 status code for invalid id', async () => {
+		const res = await request(app).get('/contributors/someInvalidID')
+		expect(res.status).toEqual(404)
+		expect(res.type).toEqual("application/json")
+		expect(res.body.message).toEqual("There is no contributor with the ID that you requested.")
+	})
 });
 
