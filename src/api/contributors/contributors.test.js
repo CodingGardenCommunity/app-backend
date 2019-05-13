072c1a1e025e9fae9bca867c83355a739afd5383
spiray <spira.joe214@gmail.com>
2019-05-13 13:32:31 -0400

    destructured respons eon contrib tests

--- src/api/contributors/contributors.test.js
+++ src/api/contributors/contributors.test.js
@@ -4,25 +4,25 @@ const app = require('../../app');
 
 describe('GET /contributors', () => {
   it('Should respond with a 200 status code', async () => {
-    const res = await request(app).get('/contributors');
-    expect(res.status).toEqual(200);
-    expect(res.type).toEqual('application/json');
-    expect(res.body.data.length).toBeGreaterThanOrEqual(0);
+    const { status, type, body: { data } } = await request(app).get('/contributors');
+    expect(status).toEqual(200);
+    expect(type).toEqual('application/json');
+    expect(data.length).toBeGreaterThanOrEqual(0);
   });
 });
 
 describe('GET /contributors/:id', () => {
   it('Should respond with a 200 status code for valid id', async () => {
-    const res = await request(app).get('/contributors/w3cj');
-    expect(res.status).toEqual(200);
-    expect(res.type).toEqual('application/json');
-    expect(res.body.data[0].id).toEqual('w3cj');
+    const { status, type, body: { data } } = await request(app).get('/contributors/w3cj');
+    expect(status).toEqual(200);
+    expect(type).toEqual('application/json');
+    expect(data[0].id).toEqual('w3cj');
   });
 
   it('Should respond with 404 status code for invalid id', async () => {
-    const res = await request(app).get('/contributors/someInvalidID');
-    expect(res.status).toEqual(404);
-    expect(res.type).toEqual('application/json');
-    expect(res.body.message).toEqual('There is no contributor with the ID that you requested.');
+    const { status, type, body: { message } } = await request(app).get('/contributors/someInvalidID');
+    expect(status).toEqual(404);
+    expect(type).toEqual('application/json');
+    expect(message).toEqual('There is no contributor with the ID that you requested.');
   });
 });
