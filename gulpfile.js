08ed643a66d305dbcfbf5f5e8cb4e6cbdf20d509
spiray <spira.joe214@gmail.com>
2019-05-30 14:29:37 -0400

    Modified gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 const gulp = require('gulp');
 const apidoc = require('gulp-apidoc');
 
-gulp.task('apidoc', (done) => {
-  apidoc({
-    src: './src',
-    dest: './apidoc',
-  }, done);
-});
+gulp.task('apidoc', done => apidoc({ src: './src', dest: './apidoc' }, done));
 
-gulp.task('watch', () => {
-  gulp.watch(['./src/**'], ['apidoc']);
-});
+gulp.task('watch', () => gulp.watch(['./src/**'], ['apidoc']));
