00437132f1908a44e975b998ead43f6b7e0963ad
Jaman Brundage <jbrundage372@gmail.com>
2019-06-16 23:47:55 -0400

    Adds video api endpoints
    Signed-off-by: Jaman Brundage <jbrundage372@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const contributors = require('./api/contributors/contributors.routes');
 const faq = require('./api/faq/faq.routes');
 const admin = require('./api/admin/admin.routes');
 const history = require('./api/history/history.routes');
+const video = require('./api/video/video.routes');
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/contributors', contributors);
 router.use('/faq', faq);
 router.use('/admin', isAdmin, admin);
 router.use('/history', history);
+router.use('/video', video);
 
 module.exports = router;
