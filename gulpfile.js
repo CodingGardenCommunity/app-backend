b1346dc53f8df9b1f476d26cb75676084e2f3339
spiray <spira.joe214@gmail.com>
2019-06-25 12:44:01 -0400

    Removed docs and left prettier

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const apidoc = require('gulp-apidoc');
-
-gulp.task('apidoc', done => apidoc({ src: './src', dest: './apidoc' }, done));
-
-gulp.task('watch', () => gulp.watch(['./src/**'], ['apidoc']));
