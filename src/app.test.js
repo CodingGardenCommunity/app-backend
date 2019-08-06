e7c85aae4eb729c5580f5fd629cd359525089199
spiray <spira.joe214@gmail.com>
2019-08-06 17:02:32 -0400

    Fix for open handles on tests

--- src/app.test.js
+++ src/app.test.js
@@ -3,12 +3,13 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('Should respond with a message', async () => {
+  it('Should respond with a message', async done => {
     const msg = 'Check out /contributors, /faq, /history and /video';
     const response = await request(app)
       .get('/')
       .expect('Content-Type', /json/)
       .expect(200);
     expect(response.body.msg).toBe(msg);
+    done();
   });
 });
