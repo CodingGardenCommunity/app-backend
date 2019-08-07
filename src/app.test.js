36086931762801cd69dac0516de1c69bf09eed4c
spiray <spira.joe214@gmail.com>
2019-08-07 09:43:41 -0400

    Final change

--- src/app.test.js
+++ src/app.test.js
@@ -4,12 +4,10 @@ const app = require('./app');
 
 describe('GET /', () => {
   it('Should respond with a message', async done => {
-    const msg = 'Check out /contributors, /faq, /history and /video';
-    const response = await request(app)
+    const msg = { msg: 'Check out /contributors, /faq, /history and /video' };
+    request(app)
       .get('/')
       .expect('Content-Type', /json/)
-      .expect(200);
-    expect(response.body.msg).toBe(msg);
-    done();
+      .expect(200, msg, done);
   });
 });
