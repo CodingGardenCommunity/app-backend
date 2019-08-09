ea238dbfc00c82cd4a00ca5b64805cfef240481b
Tejas H <tejas007tez@gmail.com>
2019-08-09 22:27:15 +0530

    Minor improvements.

--- src/api/docs/doc.test.js
+++ src/api/docs/doc.test.js
@@ -3,9 +3,12 @@ const { PORT } = require('../../config');
 const app = require('../../app').listen(PORT);
 
 let browser;
+let page;
+const BASE_URL = `http://localhost:${PORT}`;
 
 beforeAll(async () => {
   browser = await puppeteer.launch();
+  page = await browser.newPage();
 });
 
 afterAll(async () => {
@@ -15,20 +18,14 @@ afterAll(async () => {
 
 describe('GET /apiDocs/', () => {
   it('Should display "Available versions" text on page', async () => {
-    const page = await browser.newPage();
-
-    await page.goto(`http://localhost:${PORT}/apiDocs/`);
-
+    await page.goto(`${BASE_URL}/apiDocs/`);
     await expect(page).toMatch('Available versions');
   });
 });
 
 describe('GET /apiDocs/v1.0.0', () => {
   it('Should display "CodingGarden Community App APIs" text on page', async () => {
-    const page = await browser.newPage();
-
-    await page.goto(`http://localhost:${PORT}/apiDocs/v1.0.0`);
-
+    await page.goto(`${BASE_URL}/apiDocs/v1.0.0`);
     await expect(page).toMatch('CodingGarden Community App APIs');
   });
 });
