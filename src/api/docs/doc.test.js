d754d708cab726d4b9675dab9748a9a4b07b1a3a
Tejas H <tejas007tez@gmail.com>
2019-08-09 21:05:36 +0530

    Updated doc.test.js file.

--- src/api/docs/doc.test.js
+++ src/api/docs/doc.test.js
@@ -1,18 +1,16 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
+require('../../app');
+
+const { PORT } = require('../../config');
+require('../../index');
 
 describe('GET /apiDocs/', () => {
   it('Should display "Available versions" text on page', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    if (process.env.NODE_ENV === 'test') {
-      await page.goto('http://localhost:3000/apiDocs/');
-    } else if (process.env.NODE_ENV === 'development') {
-      await page.goto('https://api-dev.coding.garden/apiDocs/');
-    } else if (process.env.NODE_ENV === 'production') {
-      await page.goto('https://api.coding.garden/apiDocs/');
-    }
+    await page.goto(`http://localhost:${PORT}/apiDocs/`);
 
     await expect(page).toMatch('Available versions');
     await browser.close();
@@ -24,13 +22,7 @@ describe('GET /apiDocs/v1.0.0', () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    if (process.env.NODE_ENV === 'test') {
-      await page.goto('http://localhost:3000/apiDocs/v1.0.0');
-    } else if (process.env.NODE_ENV === 'development') {
-      await page.goto('https://api-dev.coding.garden/apiDocs/v1.0.0');
-    } else if (process.env.NODE_ENV === 'production') {
-      await page.goto('https://api.coding.garden/apiDocs/v1.0.0');
-    }
+    await page.goto(`http://localhost:${PORT}/apiDocs/v1.0.0`);
 
     await expect(page).toMatch('CodingGarden Community App APIs');
     await browser.close();
