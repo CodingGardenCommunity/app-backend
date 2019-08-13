712530a9da336e9f9f2715daad1fe0c90d07b21c
spiray <spira.joe214@gmail.com>
2019-08-13 14:40:53 -0400

    fixed test

--- src/api/admin/admin.test.js
+++ src/api/admin/admin.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('GET /admin', () => {
-  it('Should respond with a 401 status code', done => {
+  it('Should respond with a 404 status code', done => {
     request(app)
       .get('/admin')
       .expect('Content-Type', /json/)
