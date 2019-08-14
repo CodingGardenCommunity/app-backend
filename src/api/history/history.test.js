22845203de3d1c900e1c6654f3c83ec1d4c88533
Tejas H <tejas007tez@gmail.com>
2019-08-15 00:34:41 +0530

    Added tests for /history/:id endpoint.

--- src/api/history/history.test.js
+++ src/api/history/history.test.js
@@ -9,3 +9,25 @@ describe('GET /history', () => {
       .expect('Content-Type', /json/)
       .expect(200, done));
 });
+
+describe('GET /history/:id', () => {
+  it('Should respond with a 200 status code for valid id', async done => {
+    const { status, type, body } = await request(app).get('/history/5d53a0cfbb779e2988d67d0f');
+    expect(status).toEqual(200);
+    expect(type).toEqual('application/json');
+    expect(body[0].id).toEqual('5d53a0cfbb779e2988d67d0f');
+    done();
+  });
+
+  it('Should respond with 404 status code for invalid id', async done => {
+    const {
+      status,
+      type,
+      body: { message },
+    } = await request(app).get('/history/someInvalidID');
+    expect(status).toEqual(404);
+    expect(type).toEqual('application/json');
+    expect(message).toEqual('Invalid History ID.');
+    done();
+  });
+});
