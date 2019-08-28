1e35423b00a0f24c160e89fb93f3a21dfa8c80f1
spiray <spira.joe214@gmail.com>
2019-08-28 16:56:22 -0400

    modified open coverage script

--- src/tasks/openCoverage.js
+++ src/tasks/openCoverage.js
@@ -1,6 +1,27 @@
+const { platform } = require('os');
+const {
+  promises: { stat },
+} = require('fs');
 const { exec } = require('child_process');
 
 const file = 'coverage/lcov-report/index.html';
 
-if (process.platform === 'win32') exec(`start ${file}`);
-else exec(`open ${file}`);
+(async () => {
+  try {
+    await stat(file);
+    if (platform() === 'win32') exec(`start ${file}`);
+    else if (platform() === 'darwin') exec(`open ${file}`);
+    else exec(`xdg-open ${file}`);
+  } catch (err) {
+    try {
+      exec('npm run test', () => {
+        if (platform() === 'win32') exec(`start ${file}`);
+        else if (platform() === 'darwin') exec(`open ${file}`);
+        else exec(`xdg-open ${file}`);
+      });
+    } catch (error) {
+      process.stdout.write(err);
+      process.exit(1);
+    }
+  }
+})();
