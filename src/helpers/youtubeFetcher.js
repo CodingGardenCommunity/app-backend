29460d880efc30f0c0066a4bf6ce9b5229d9889d
nicomigueles <nicolasmigueles99@gmail.com>
2019-08-29 15:54:23 -0300

    youtubeFetcher refactored

--- src/helpers/youtubeFetcher.js
+++ src/helpers/youtubeFetcher.js
@@ -2,29 +2,33 @@ const fetch = require('node-fetch');
 const { YOUTUBE_API_KEY, YOUTUBE_CHANNEL_ID } = require('../config');
 
 function transformYoutubeResponseData({ items }) {
-  return items.map(({ snippet: { id: { videoID }, title, publishedAt, description, thumbnails: { high } } }) => {
+  return items.map(({ id: { videoID }, snippet: { title, publishedAt, description, thumbnails: { high: { url } } } }) => {
     return {
       type: 'video',
-      name: title,
-      date: publishedAt,
-      url: `https://www.youtube.com/watch?v=${videoID}`,
-      thumbnail: high,
-      videoID,
-      title,
-      description,
+      id: videoID,
+      attributes: {
+        type: 'video',
+        name: title,
+        videoID,
+        title,
+        date: publishedAt,
+        description,
+        url: `https://www.youtube.com/watch?v=${videoID}`,
+        thumbnail: url,
+      },
     };
   });
 }
 
-export default async function fetchLatestYoutubeVideos() {
-  const url = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${YOUTUBE_CHANNEL_ID}&part=snippet&order=date&maxResults=50`;
+async function fetchLatestYoutubeVideos(maxResults) {
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${YOUTUBE_CHANNEL_ID}&part=snippet&order=date&maxResults=${maxResults}`;
   try {
     const resp = await fetch(url);
     const data = await resp.json();
     return transformYoutubeResponseData(data);
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    return [];
+  } catch (err) {
+    if (err) throw new Error(err);
+    return err;
   }
 }
+module.exports = fetchLatestYoutubeVideos;
