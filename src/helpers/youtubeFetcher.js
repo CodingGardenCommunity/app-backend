7f0057f6d95626dda8ad9ae279e82568049e10ac
nicomigueles <nicolasmigueles99@gmail.com>
2019-08-29 17:16:47 -0300

    all in one function

--- src/helpers/youtubeFetcher.js
+++ src/helpers/youtubeFetcher.js
@@ -1,34 +1,32 @@
 const fetch = require('node-fetch');
 const { YOUTUBE_API_KEY, YOUTUBE_CHANNEL_ID } = require('../config');
 
-function transformYoutubeResponseData({ items }) {
-  return items.map(({ id: { videoID }, snippet: { title, publishedAt, description, thumbnails: { high: { url } } } }) => {
-    return {
-      type: 'video',
-      id: videoID,
-      attributes: {
-        type: 'video',
-        name: title,
-        videoID,
-        title,
-        date: publishedAt,
-        description,
-        url: `https://www.youtube.com/watch?v=${videoID}`,
-        thumbnail: url,
-      },
-    };
-  });
-}
-
 async function fetchLatestYoutubeVideos(maxResults) {
-  const url = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${YOUTUBE_CHANNEL_ID}&part=snippet&order=date&maxResults=${maxResults}`;
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${YOUTUBE_CHANNEL_ID}&part=snippet&order=date&maxResults=${maxResults}`;
   try {
-    const resp = await fetch(url);
-    const data = await resp.json();
-    return transformYoutubeResponseData(data);
+    const resp = await fetch(apiUrl);
+    const { items } = await resp.json();
+    return items.map(({ id: { videoID }, snippet: { title, publishedAt, description, thumbnails: { high: { url } } } }) => {
+      return {
+        type: 'video',
+        id: videoID,
+        attributes: {
+          type: 'video',
+          name: title,
+          videoID,
+          title,
+          date: publishedAt,
+          description,
+          url: `https://www.youtube.com/watch?v=${videoID}`,
+          thumbnail: url,
+        },
+      };
+    });
   } catch (err) {
-    if (err) throw new Error(err);
-    return err;
+    // eslint-disable-next-line no-console
+    console.log(err);
+    return [];
   }
 }
+
 module.exports = fetchLatestYoutubeVideos;
