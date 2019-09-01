f43bb2916a2dcd34c2b4f217a0a0cb55f988cef3
nicomigueles <nicolasmigueles99@gmail.com>
2019-09-01 00:50:33 -0300

    clean up + minor bugs correction

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -6,10 +6,16 @@ async function fetchLatestYoutubeVideos(options) {
   const paOpt = 'publishedAfter' in options ? `publishedAfter=${options.publishedAfter}` : '';
   const optUrl = [mrOpt, paOpt].join('&') || '';
   const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${YOUTUBE_CHANNEL_ID}&part=snippet&order=date&${optUrl}`;
-
   try {
     const resp = await fetch(apiUrl);
-    const { items } = await resp.json();
+    const { items, error } = await resp.json();
+
+    if (error && error.errors[0].reason === 'keyInvalid') {
+      // eslint-disable-next-line no-console
+      console.log(`Invalid 'YOUTUBE_API_KEY'`);
+      return [];
+    }
+
     return items.map(({ id: { videoId: videoID }, snippet: { title, publishedAt: date, description, thumbnails: { high: { url: thumbnail } } } }) => {
       return {
         type: 'video',
--- src/tasks/pollYoutube.js
+++ src/tasks/pollYoutube.js
@@ -1,12 +1,12 @@
+const { red, green } = require('colors/safe');
 const { schedule } = require('node-cron');
 const { fetchLatestYoutubeVideos } = require('../helpers/fetchData');
 const Video = require('../api/video/video.model');
 
 schedule('59 * * * *', async () => {
   try {
-    let log = false;
     let { date: lastDate } = await Video.findOne({}).sort({ date: -1 });
-    lastDate = lastDate.toISOString();
+    lastDate = lastDate.toISOString(); // Transforms date formart to the Youtube-API Standar.
 
     const fetchedVideos = await fetchLatestYoutubeVideos({ publishedAfter: lastDate });
     const now = new Date().toISOString();
@@ -16,19 +16,12 @@ schedule('59 * * * *', async () => {
         if (newVideo.date !== lastDate) {
           const { name } = await new Video(newVideo).save();
           // eslint-disable-next-line no-console
-          console.log(`[CRON-JOB] Added new video from Youtube: ${name}, at ${now}`);
-        } else {
-          log = true;
+          console.log(green(`[cron-job] Added new video from Youtube: ${name}, at ${now}`));
         }
       });
-      // eslint-disable-next-line no-console
-      if (log) console.log(`[CRON-JOB] No new videos found, at ${now}`);
-    } else {
-      // eslint-disable-next-line no-console
-      console.log(`[CRON-JOB] No new videos found, at ${now}`);
     }
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error(`[CRON-JOB][ERROR] ${err}`);
+    console.error(red(`[cron-job-error] ${err}`));
   }
 });
