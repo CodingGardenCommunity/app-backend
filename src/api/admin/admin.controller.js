fc54a3eafc1a99c7cd2ea1e22a02bd5c54792bd8
nicomigueles <nicolasmigueles99@gmail.com>
2019-09-06 21:00:52 -0300

    Added `admin/fetch-videos` endpoint to manually fetch the yt-api

--- src/api/admin/admin.controller.js
+++ src/api/admin/admin.controller.js
@@ -1,4 +1,5 @@
 const { mainSeed } = require('../../tasks/seed/seed');
+const { fetchVideosJob } = require('../../helpers/fetchData');
 
 async function seed(req, res, next) {
   try {
@@ -9,6 +10,18 @@ async function seed(req, res, next) {
   }
 }
 
+async function fetchVideos(req, res, next) {
+  try {
+    // eslint-disable-next-line no-console
+    console.log('Manual fetch.');
+    await fetchVideosJob();
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   seed,
+  fetchVideos,
 };
