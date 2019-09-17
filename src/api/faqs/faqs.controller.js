21329fa162065cae86bf7eeb6b32ca9e7fc07866
spiray <spira.joe214@gmail.com>
2019-09-17 09:32:22 -0400

    :ok_hand: minor change

--- src/api/faqs/faqs.controller.js
+++ src/api/faqs/faqs.controller.js
@@ -1,4 +1,4 @@
-const FAQs = require('./faqs.model');
+const FAQ = require('./faqs.model');
 
 async function getFAQs(req, res, next) {
   try {
@@ -6,12 +6,12 @@ async function getFAQs(req, res, next) {
     if ('id' in req.params) {
       if (!/^[a-fA-F0-9]{24}$/.test(req.params.id)) throw new ReferenceError('Invalid FAQ ID.');
       try {
-        response = [await FAQs.findById(req.params.id).exec()];
+        response = [await FAQ.findById(req.params.id).exec()];
         if (response[0] === null) throw new ReferenceError('The requested ID does not exist.');
       } catch ({ message }) {
         throw new ReferenceError(message);
       }
-    } else response = await FAQs.find({}).exec();
+    } else response = await FAQ.find({}).exec();
 
     const finalResponse = response.map(({ id, question, answer, createdAt, updatedAt }) => ({
       type: 'faq',
@@ -34,7 +34,7 @@ async function addFAQ(req, res, next) {
   try {
     const { question, answer } = req.body;
     if (!question || !answer) throw new ReferenceError('Make sure your request includes a question and answer.');
-    const { _id } = await new FAQs({ question, answer }).save();
+    const { _id } = await new FAQ({ question, answer }).save();
     res.json({
       status: 200,
       message: `FAQ with ID: ${_id} has been added successfully to the DB.`,
@@ -49,7 +49,7 @@ async function removeFAQ(req, res, next) {
   try {
     const { id: _id } = req.params;
     if (!/^[a-fA-F0-9]{24}$/.test(_id)) throw new ReferenceError('Invalid FAQ ID.');
-    const deletedFAQ = await FAQs.deleteOne({ _id });
+    const deletedFAQ = await FAQ.deleteOne({ _id });
     if (deletedFAQ.deletedCount === 0) throw new ReferenceError('There is no FAQ to delete with that ID.');
     res.json({
       status: 200,
